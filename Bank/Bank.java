6725f30a7bc51e560243551654d448a702b5c417
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-01-10 11:50:15 -0600

    Create Bank.java

---
+++ Bank/Bank.java
@@ -0,0 +1,9 @@
+package Bank;
+
+//Abstract class Bank
+//Not instantiated
+//Acts as a contract for subclass
+public abstract class Bank {
+    // Abstract method (no body)
+    abstract double getRateOfInterest();
+}
