0e69925d3b9c5c2377c64f9f9ec5e2d7e0dd39b3
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-01-10 11:51:28 -0600

    Delete Bank directory

--- Bank/Bank.java
+++
@@ -1,9 +0,0 @@
-package Bank;
-
-//Abstract class Bank
-//Not instantiated
-//Acts as a contract for subclass
-public abstract class Bank {
-    // Abstract method (no body)
-    abstract double getRateOfInterest();
-}
